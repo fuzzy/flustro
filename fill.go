dffecb0af8c8c3daa994ef99aee1fdc4397cae81
Mike 'Fuzzy' Partin <fuzzy@fumanchu.org>
2016-09-08 00:26:44 -0700

    wip

--- fill.go
+++ fill.go
@@ -6,6 +6,7 @@ import (
 	"runtime"
 	"time"
 
+	"github.com/kisielk/whisper-go/whisper"
 	"github.com/urfave/cli"
 )
 
@@ -19,11 +20,70 @@ type Dirstate struct {
 	Contents map[string][]string
 }
 
+type Overlap struct {
+	Source      string
+	Destination string
+	Contents    map[string][]string
+}
+
 func Filler() {
 	for {
 		select {
 		case msg := <-DataChan:
 			Debug <- fmt.Sprintf("Backfill: %s -> %s", msg["Source"], msg["Destination"])
+
+			// Open our filehandles
+			sDb, sErr := whisper.Open(msg["Source"])
+			dDb, dErr := whisper.Open(msg["Destination"])
+			if !chkErr(sErr) {
+				Error <- fmt.Sprintf("%s: %s", msg["Source"], sErr.Error())
+			} else if !chkErr(dErr) {
+				Error <- fmt.Sprintf("%s: %s", msg["Destination"], dErr.Error())
+				os.Exit(1)
+			}
+			// Defer their closings
+			//defer sDb.Close()
+			//defer dDb.Close()
+
+			// Now for a series of checks, first to ensure that both
+			// files have the same number of archives in them.
+			if sDb.Header.Metadata.ArchiveCount != dDb.Header.Metadata.ArchiveCount {
+				Error <- fmt.Sprintln("The files have a mismatched set of archives.")
+			} else {
+				// Now we'll start processing the archives, checking as we go to see if they
+				// are matched. That way we at least fill in what we can, possibly....
+				for i, a := range sDb.Header.Archives {
+					// The offset
+					if a.Offset == dDb.Header.Archives[i].Offset {
+						// and the number of points
+						if a.Points == dDb.Header.Archives[i].Points {
+							// and finally the interval
+							if a.SecondsPerPoint == dDb.Header.Archives[i].SecondsPerPoint {
+								// ok, now let's get rolling through the archives
+								sp, se := sDb.DumpArchive(i)
+								if se != nil {
+									Error <- fmt.Sprintf("%s: %s", msg["Source"], se.Error())
+									os.Exit(1)
+								}
+								dp, de := dDb.DumpArchive(i)
+								if de != nil {
+									Error <- fmt.Sprintln(de.Error())
+									os.Exit(1)
+								}
+								for idx := 0; idx < len(sp); idx++ {
+									if sp[idx].Timestamp != 0 && sp[idx].Value != 0 {
+										if dp[idx].Timestamp == 0 || dp[idx].Value == 0 {
+											dp[idx].Timestamp = sp[idx].Timestamp
+											dp[idx].Value = sp[idx].Value
+											dDb.Update(dp[idx])
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
 		case <-FillerSig:
 			FillerSig <- true
 			return
@@ -53,17 +113,38 @@ func BackFill(c *cli.Context) {
 			}
 			// next we'll start processing through our srcObj and dstObj lists and
 			// backfill everything that's present in both locations
+			overlap := Overlap{
+				Source:      srcObj.Location,
+				Destination: dstObj.Location,
+				Contents:    make(map[string][]string),
+			}
+			overlap_c := 0
 			for k, _ := range srcObj.Contents {
 				if _, ok := dstObj.Contents[k]; ok {
 					for _, v := range srcObj.Contents[k] {
 						for _, dv := range dstObj.Contents[k] {
 							if v == dv {
-								DataChan <- map[string]string{
-									"Source":      fmt.Sprintf("%s/%s/%s", srcObj.Location, k, v),
-									"Destination": fmt.Sprintf("%s/%s/%s", dstObj.Location, k, v),
+								if _, ok := overlap.Contents[k]; ok {
+									overlap.Contents[k] = append(overlap.Contents[k], v)
+									overlap_c++
+								} else {
+									overlap.Contents[k] = []string{v}
+									overlap_c++
 								}
 							}
 						}
+					}
+				}
+			}
+			Info <- fmt.Sprintf("%d entries shared between %s and %s.",
+				overlap_c,
+				overlap.Source,
+				overlap.Destination)
+			for k, v := range overlap.Contents {
+				for _, f := range v {
+					DataChan <- map[string]string{
+						"Source":      fmt.Sprintf("%s/%s/%s", overlap.Source, k, f),
+						"Destination": fmt.Sprintf("%s/%s/%s", overlap.Destination, k, f),
 					}
 				}
 			}
@@ -217,7 +298,7 @@ func backfillFile(s string, d string) bool {
 								dp[idx].Timestamp = sp[idx].Timestamp
 								dp[idx].Value = sp[idx].Value
 								dDb.Update(dp[idx])
-							}
+						}
 						}
 					}
 				}
@@ -311,7 +392,7 @@ func Filler(c *cli.Context) error {
 }
 */
 func init() {
-	DataChan = make(chan map[string]string)
+	DataChan = make(chan map[string]string, 8192)
 	Commands = append(Commands, cli.Command{
 		Name:        "fill",
 		Aliases:     []string{"f"},

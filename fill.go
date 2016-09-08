4a18d698b826e9f873975ecda67d5356e565b5ba
Mike 'Fuzzy' Partin <fuzzy@fumanchu.org>
2016-09-08 11:05:17 -0700

    removed old code, new code is fully functional.

--- fill.go
+++ fill.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+	"sync"
 	"time"
 
 	"github.com/kisielk/whisper-go/whisper"
@@ -13,6 +14,8 @@ import (
 var (
 	DataChan  chan map[string]string
 	FillerSig chan bool
+	Count     int32
+	Mutex     *sync.Mutex
 )
 
 type Dirstate struct {
@@ -42,8 +45,8 @@ func Filler() {
 				os.Exit(1)
 			}
 			// Defer their closings
-			//defer sDb.Close()
-			//defer dDb.Close()
+			defer sDb.Close()
+			defer dDb.Close()
 
 			// Now for a series of checks, first to ensure that both
 			// files have the same number of archives in them.
@@ -84,6 +87,9 @@ func Filler() {
 					}
 				}
 			}
+			Mutex.Lock()
+			Count++
+			Mutex.Unlock()
 		case <-FillerSig:
 			FillerSig <- true
 			return
@@ -140,6 +146,8 @@ func BackFill(c *cli.Context) {
 				overlap_c,
 				overlap.Source,
 				overlap.Destination)
+			time.Sleep(500 * time.Millisecond)
+			start_t := int32(time.Now().Unix())
 			for k, v := range overlap.Contents {
 				for _, f := range v {
 					DataChan <- map[string]string{
@@ -147,6 +155,19 @@ func BackFill(c *cli.Context) {
 						"Destination": fmt.Sprintf("%s/%s/%s", overlap.Destination, k, f),
 					}
 				}
+			}
+			for {
+				runtime := (int32(time.Now().Unix()) - start_t)
+				if len(DataChan) == 0 {
+					time.Sleep(2 * time.Second)
+					runrate := (float32(overlap_c) / float32(runtime))
+					Info <- fmt.Sprintf("%d files processed in %d sec @ %.02f/sec.", overlap_c, runtime, runrate)
+					time.Sleep(100 * time.Millisecond)
+					return
+				}
+				runrate := (float32(Count) / float32(runtime))
+				Progress <- fmt.Sprintf("%d files processed in %d sec @ %.02f/sec.", Count, runtime, runrate)
+				time.Sleep(1 * time.Second)
 			}
 		} else if isFile(srcDir) && isFile(dstDir) {
 			// we only need one worker for this job
@@ -164,235 +185,9 @@ func BackFill(c *cli.Context) {
 	return
 }
 
-/*
-import (
-	"errors"
-	"fmt"
-	"os"
-	"path"
-	"runtime"
-	"sync"
-	"time"
-
-	"github.com/dustin/go-humanize"
-	"github.com/fuzzy/gcl"
-	"github.com/kisielk/whisper-go/whisper"
-	"github.com/urfave/cli"
-)
-
-type FillState struct {
-	StartTime int64
-	Count     int64
-	CountLock sync.Mutex
-	CountChan chan bool
-	SrcTotal  int64
-	DstTotal  int64
-	Overlap   int64
-}
-
-func (f FillState) Increment() {
-	f.CountLock.Lock()
-	f.Count++
-	f.CountChan <- true
-	f.CountLock.Unlock()
-}
-
-func (f FillState) DumpState() {
-	// grab our lock, this ensures accurate reporting, and gives a bit more
-	// of a concurrency throttle. This is not necessarily a bad thing.
-	f.CountLock.Lock()
-	r := (time.Now().Unix() - f.StartTime)
-	c := f.Count
-	if r > 0 {
-		c = (f.Count / r)
-	}
-	p := (float64(f.Count) / float64(f.Overlap)) * 100.00
-	fmt.Printf("%s %-6s/%-6s (%6.02f%%) in %d seconds @ %-6s/sec",
-		gcl.String("Info").Green().Bold(),
-		humanize.Comma(f.Count),
-		humanize.Comma(f.Overlap),
-		p, HumanTime(int(r)), c)
-	f.CountLock.Unlock()
-}
-
-var (
-	State FillState
-)
-
-func stateWorker(t chan bool) {
-	for {
-		select {
-		case sig := <-State.CountChan:
-			if sig {
-				State.DumpState()
-			} else {
-				return
-			}
-		case <-t:
-			return
-		}
-	}
-}
-
-func fillWorker(d chan map[string]string, t chan bool) {
-	for {
-		msg := <-d
-		if msg["SRC"] == "__EXIT__" && msg["DST"] == "__EXIT__" {
-			return
-		} else {
-			if isFile(msg["SRC"]) && isFile(msg["DST"]) {
-				if !backfillFile(msg["SRC"], msg["DST"]) {
-					Error.Printf("S:%s D:%s - Backfill operation failed.",
-						msg["SRC"],
-						msg["DST"])
-					os.Exit(1)
-				} else {
-					State.Increment()
-				}
-			}
-		}
-	}
-}
-
-func backfillFile(s string, d string) bool {
-	// Open our filehandles
-	sDb, sErr := whisper.Open(s)
-	dDb, dErr := whisper.Open(d)
-	if !chkErr(sErr) || !chkErr(dErr) {
-		os.Exit(1)
-	}
-	// Defer their closings
-	defer sDb.Close()
-	defer dDb.Close()
-
-	// Now for a series of checks, first to ensure that both
-	// files have the same number of archives in them.
-	if sDb.Header.Metadata.ArchiveCount != dDb.Header.Metadata.ArchiveCount {
-		Error.Println("The files have a mismatched set of archives.")
-		return false
-	}
-
-	// Now we'll start processing the archives, checking as we go to see if they
-	// are matched. That way we at least fill in what we can, possibly....
-	for i, a := range sDb.Header.Archives {
-		// The offset
-		if a.Offset == dDb.Header.Archives[i].Offset {
-			// and the number of points
-			if a.Points == dDb.Header.Archives[i].Points {
-				// and finally the interval
-				if a.SecondsPerPoint == dDb.Header.Archives[i].SecondsPerPoint {
-					// ok, now let's get rolling through the archives
-					sp, se := sDb.DumpArchive(i)
-					if se != nil {
-						Error.Println(se.Error())
-						os.Exit(1)
-					}
-					dp, de := dDb.DumpArchive(i)
-					if de != nil {
-						Error.Println(de.Error())
-						os.Exit(1)
-					}
-					for idx := 0; idx < len(sp); idx++ {
-						if sp[idx].Timestamp != 0 && sp[idx].Value != 0 {
-							if dp[idx].Timestamp == 0 || dp[idx].Value == 0 {
-								dp[idx].Timestamp = sp[idx].Timestamp
-								dp[idx].Value = sp[idx].Value
-								dDb.Update(dp[idx])
-						}
-						}
-					}
-				}
-			}
-		}
-	}
-	return true
-}
-
-func Filler(c *cli.Context) error {
-	if len(c.Args()) == 2 {
-		args := c.Args()
-		if isDir(args[0]) && isDir(args[1]) {
-			State.Count = 0
-
-			// First things first, let's spawn our pool of workers.
-			dataCh := make(chan map[string]string, 1)
-			timeout := make(chan bool, 30)
-			go stateWorker(timeout)
-			for i := 0; i < c.Int("j"); i++ {
-				go fillWorker(dataCh, timeout)
-			}
-
-			// Let's get this dir walking in there then shall we?
-			srcFiles := listFiles(args[0])
-			dstFiles := listFiles(args[1])
-
-			// Now let's find all our overlap
-			overlap := []string{}
-			for a := 0; a < len(srcFiles); a++ {
-				for b := 0; b < len(dstFiles); b++ {
-					if srcFiles[a] == dstFiles[b] {
-						overlap = append(overlap, srcFiles[a])
-					}
-				}
-			}
-
-			// And display some stats
-			Info.Printf("srcDir: %d files, dstDir: %d files, %d overlap.",
-				len(srcFiles),
-				len(dstFiles),
-				len(overlap))
-			State.Overlap = int64(len(overlap))
-			State.SrcTotal = int64(len(srcFiles))
-			State.DstTotal = int64(len(dstFiles))
-
-			// Now we can push in all our data, and let our workers do their
-			// lovely little thing. Ahhhhh concurrency.
-			StartTime = time.Now().Unix()
-			for i := 0; i < len(overlap); i++ {
-				dataCh <- map[string]string{
-					"SRC": fmt.Sprintf("%s/%s", args[0], overlap[i]),
-					"DST": fmt.Sprintf("%s/%s", args[1], overlap[i]),
-				}
-			}
-
-			// And while they're off doing that, here we will just sit and watch
-			// the return channel and count things. Once we have all our
-			// backfill operations accounted for, we can reap all of our workers
-			// and carry on.
-			for State.Count < int64(len(overlap)) {
-				time.Sleep(1 * time.Second)
-			}
-			// And finally let's reap all our children
-			for idx := 0; idx < c.Int("j"); idx++ {
-				dataCh <- map[string]string{
-					"SRC": "__EXIT__",
-					"DST": "__EXIT__",
-				}
-			}
-
-			fmt.Println("")
-			Info.Println((State.Overlap / (time.Now().Unix() - StartTime)),
-				"whisper files processed per second.")
-		} else {
-			if !backfillFile(args[0], args[1]) {
-				e := fmt.Sprintf("Error while backfilling.")
-				Error.Println(e)
-				return errors.New(e)
-			}
-		}
-	} else {
-		var e string
-		e = fmt.Sprintf("Wrong number of paramters given.")
-		Error.Println(e)
-		Error.Printf("Try '%s help fill' for more information",
-			path.Base(os.Args[0]))
-		return errors.New(e)
-	}
-	return nil
-}
-*/
 func init() {
 	DataChan = make(chan map[string]string, 8192)
+	Mutex = &sync.Mutex{}
 	Commands = append(Commands, cli.Command{
 		Name:        "fill",
 		Aliases:     []string{"f"},
@@ -403,7 +198,7 @@ func init() {
 			cli.IntFlag{
 				Name:  "j",
 				Usage: "Number of workers (for directory recursion)",
-				Value: runtime.GOMAXPROCS(0),
+				Value: (runtime.GOMAXPROCS(0) * 2),
 			},
 		},
 		SkipFlagParsing: false,
